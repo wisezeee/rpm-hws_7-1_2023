bbaa26763a387b5837fa675f5c5ba226f2c28cb3
wisezeee <113104715+wisezeee@users.noreply.github.com>
2023-04-17 13:25:53 +0300

    Delete covid_tracker directory

--- covid_tracker/migrations/init.sql
+++
@@ -1,9 +0,0 @@
-create table population (
-    id int generated always as identity primary key,
-    population_ru text,
-    population_en text,
-    population_world text
-);
-
-insert into population (population_ru, population_en, population_world)
-values ('143,4 million', '331,9 million', '7,888 milliard')
